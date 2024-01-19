f1974c3ea364dd32a6dcb4064ee6ca55b8081b7c
h1lsx <139397050+h1lsx@users.noreply.github.com>
2024-01-19 08:22:42 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -422,34 +422,11 @@ const precachedResources = [
 "/snd/toneTick.mp3",
 "/snd/upgrade.mp3",
 ];
-
-async function precache() {
-  const cache = await caches.open(cacheName);
-  return cache.addAll(precachedResources);
-}
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(precache());
-});
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  if (cachedResponse) {
-    return cachedResponse;
-  }
-  try {
-    const networkResponse = await fetch(request);
-    if (networkResponse.ok) {
-      const cache = await caches.open(cacheName);
-      cache.put(request, networkResponse.clone());
-    }
-    return networkResponse;
-  } catch (error) {
-    return Response.error();
-  }
-}
-
-self.addEventListener("fetch", (event) => {
-  if (precachedResources.includes(url.pathname)) {
-    event.respondWith(cacheFirst(event.request));
-  }
+self.addEventListener("install", event => {
+   event.waitUntil(
+      caches.open(cacheName)
+      .then(cache => {
+         return cache.addAll(precachedResources);
+      });
+   );
 });
