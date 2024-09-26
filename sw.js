af7244bdcfb357f07d8a599e41783f0e2e359278
h1lsx <hererr2@gmail.com>
2024-09-26 08:05:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -430,3 +430,23 @@ self.addEventListener("install", event => {
       });
    );
 });
+async function networkFirst(request) {
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (url.pathname.match(/^\/inbox/)) {
+    event.respondWith(networkFirst(event.request));
+  }
+});
